c660af7d59735da016bf56f6cd8ffa349c977cd2
Rob Murphy <rmurphyswimmer@gmail.com>
2025-07-12 18:06:30 +0200

    fix: startup script

--- tmux-startup-sa.zsh
+++ tmux-startup-sa.zsh
@@ -13,9 +13,9 @@ then
     tmux rename-window -t 0 'Vim'
     tmux send-keys -t 'Vim' 'cd repos/strength_assistant' C-m
     tmux send-keys -t 'Vim' 'cl' C-m
-    tmux send-keys -t 'Vim' "vim" C-m
-
-fi
+    tmux send-keys -t 'Vim' "vim ." C-m
 
 # Attach Session, on the Main window
 tmux attach-session -t $SESSION:0
+fi
+
